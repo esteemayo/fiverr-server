ab811da8c11975c3ee715d10d440bdc70b8ae8f2
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-14 04:50:02 +0100

    feat: set up the server aplication

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import compression from 'compression';
+import cors from 'cors';
+import 'colors';
+
+const app = express();
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(helmet());
+
+if (app.get('env') !== 'production') {
+  app.use(morgan('dev'));
+}
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, Please try again in 30 minutes',
+});
+
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(cookieParser());
+
+app.use(mongoSanitize());
+
+app.use(hpp());
+
+app.use(xss());
+
+app.use(compression());
+
+export default app;
---
+++ server.js
@@ -0,0 +1,22 @@
+import app from './app.js';
+import { connectDB } from './config/db.js';
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down...'.red.bold);
+  console.log(err.name, err.message);
+});
+
+app.set('port', process.env.PORT || 4000);
+
+const server = app.listen(app.get('port'), async () => {
+  await connectDB();
+  console.log(`App listening on port → ${server.address().port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 🔥 Shutting down...'.red.bold);
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
