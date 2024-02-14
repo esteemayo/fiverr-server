b66fc329262ef4bacf79dae7838a84f1c8398d1d
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-14 16:36:00 +0100

    feat: add CustomAPIError class

---
+++ errors/CustomAPIError.js
@@ -0,0 +1,10 @@
+import { StatusCodes } from 'http-status-codes';
+
+export class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+
+    this.status = 'error';
+    this.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+  }
+}
