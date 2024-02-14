4b38d7224c007bfb70c8e758b7e45b52af4faac0
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-14 18:48:18 +0100

    feat: add import-dev-data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import 'colors';
+
+import { connectDB } from '../../config/db.js';
+
+dotenv.config({ path: './variable.env' });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+connectDB();
+
+const importData = async () => {
+  try {
+    console.log(
+      '👍👍👍👍👍👍👍👍 Data successfully loaded! 👍👍👍👍👍👍👍👍'.cyan.bold,
+    );
+    process.exit();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    console.log('😢😢 Goodbye Data...');
+
+    console.log(
+      'Data successfully deleted! To load sample data, run\n\n\t npm run sample\n\n'
+        .blue.bold,
+    );
+    process.exit();
+  } catch (err) {
+    console.log(
+      '\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n'
+        .red.bold,
+    );
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else {
+  deleteData();
+}
