5967dad448ff1295112a62162a5a56b6701088fd
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-14 16:38:52 +0100

    feat: add NotFoundError class

---
+++ errors/notFound.js
@@ -0,0 +1,12 @@
+import { StatusCodes } from 'http-status-codes';
+
+import { CustomAPIError } from './CustomAPIError';
+
+export class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+
+    this.status = 'fail';
+    this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
