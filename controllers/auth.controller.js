e4f87b607e06543aa63ddf1e72923637dc92205b
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-21 19:00:34 +0100

    feat: handle login user logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,10 +3,29 @@ import asyncHandler from 'express-async-handler';
 
 import User from '../models/user.model.js';
 
+import { BadRequestError } from './../errors/badRequest.js';
+import { UnauthenticatedError } from './../errors/unauthenticated.js';
+
 export const register = asyncHandler(async (req, res, next) => {
   const user = await User.create({ ...req.body });
 
   if (user) {
     res.status(StatusCodes.CREATED).json(user);
   }
+});
+
+export const login = asyncHandler(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return next(new BadRequestError('Please provide username and password'));
+  }
+
+  const user = await User.findOne({ username }).select('+password');
+
+  if (!user || !(await user.comparePassword(password))) {
+    return next(new UnauthenticatedError('Incorrect username or password'));
+  }
+
+  return res.status(StatusCodes.OK).json(user);
 });
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.post('/register', authController.register);
 
+router.post('/login', authController.login);
+
 export default router;
