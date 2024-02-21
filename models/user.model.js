382663eb0d313351bb770172ea4dfce3d2440931
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-21 22:39:23 +0100

    fix: modify

--- models/user.model.js
+++ models/user.model.js
@@ -85,6 +85,13 @@ userSchema.pre('save', async function (next) {
   this.confirmPassword = undefined;
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
