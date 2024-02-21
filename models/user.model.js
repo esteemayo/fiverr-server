432c72233eaf91e6390cfc95d9454e0479b37f8a
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-21 18:49:18 +0100

    feat: encrypt user' password

--- models/user.model.js
+++ models/user.model.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
@@ -75,6 +76,13 @@ const userSchema = new Schema(
     timestamps: true,
   },
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+});
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
