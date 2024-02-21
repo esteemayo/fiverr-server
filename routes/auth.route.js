9f41fddf470ec77a1d99144543be0fde8ff5ea80
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-21 18:26:38 +0100

    feat: add register user route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import * as authController from '../controllers/user.controller';
+
 const router = express.Router();
+
+router.post('/register', authController.register);
 
 export default router;
