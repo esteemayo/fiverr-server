54327facd13dba942bc66c078b3e235329179487
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-21 17:48:51 +0100

    feat: setup conversatio' model

---
+++ models/conversation.model.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const conversationSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    sellerId: {
+      type: String,
+      required: true,
+    },
+    buyerId: {
+      type: String,
+      required: true,
+    },
+    readBySeller: {
+      type: Boolean,
+      required: true,
+    },
+    readByBuyer: {
+      type: Boolean,
+      required: true,
+    },
+    lastMessage: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Conversation =
+  mongoose.models.Conversation ||
+  mongoose.model('conversation', conversationSchema);
+
+export default Conversation;
