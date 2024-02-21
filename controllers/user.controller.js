94bed3769a936cd2cfd814bb6ed99c3a8fc13281
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-21 22:50:44 +0100

    feat: handle update current user' data logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,40 @@
 import { StatusCodes } from 'http-status-codes';
+import _ from 'lodash';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/user.model.js';
+import { BadRequestError } from '../errors/badRequest.js';
+import { createSendToken } from '../utils/createSendToken.js';
 
-export const updateUser = asyncHandler(async (req, res, next) => {});
+export const updateMe = asyncHandler(async (req, res, next) => {
+  const { id: userId } = req.user;
+  const { password, confirmPassword } = req.body;
+
+  if (password || confirmPassword) {
+    return next(
+      new BadRequestError(
+        `This route is not for password updates. Please use ${req.protocol}://${req.get('host')}/api/v1/auth/update-my-password`,
+      ),
+    );
+  }
+
+  const filterBody = _.pick(req.body, [
+    'username',
+    'email',
+    'image',
+    'country',
+    'phone',
+    'desc',
+  ]);
+
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    { $set: { ...filterBody } },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  createSendToken(updatedUser, StatusCodes.OK, req, res);
+});
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import * as userController from '../controllers/user.controller.js';
+
 const router = express.Router();
+
+router.patch('/update-me', userController.updateMe);
 
 export default router;
