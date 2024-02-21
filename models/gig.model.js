eec177e0c92fe7e852bc2602e56880472ffb8904
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-21 17:12:21 +0100

    fix: modify

--- models/gig.model.js
+++ models/gig.model.js
@@ -2,67 +2,72 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const gigSchema = new Schema({
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'A gig must belong to a user'],
+const gigSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'A gig must belong to a user'],
+    },
+    title: {
+      type: String,
+      required: [true, 'A gig must have a title'],
+    },
+    desc: {
+      type: String,
+      required: [true, 'A gig must have a description'],
+    },
+    totalStars: {
+      type: Number,
+      default: 0,
+    },
+    starNumber: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: [true, 'A gig must belong to a category'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'A gig must have a price'],
+    },
+    cover: {
+      type: String,
+      required: [true, 'A gig must have a cover image'],
+    },
+    images: {
+      type: [String],
+    },
+    shortTitle: {
+      type: String,
+      required: [true, 'A gig must have a short title'],
+    },
+    shortDesc: {
+      type: String,
+      required: [true, 'A gig must have a short description'],
+    },
+    deliveryTime: {
+      type: Number,
+      required: [true, 'A gig must have a delivery time'],
+    },
+    revisionNumber: {
+      type: Number,
+      required: [true, 'A gig must have a revision number'],
+    },
+    features: {
+      type: [String],
+    },
+    sales: {
+      type: Number,
+      default: 0,
+    },
   },
-  title: {
-    type: String,
-    required: [true, 'A gig must have a title'],
+  {
+    timestamps: true,
   },
-  desc: {
-    type: String,
-    required: [true, 'A gig must have a description'],
-  },
-  totalStars: {
-    type: Number,
-    default: 0,
-  },
-  starNumber: {
-    type: Number,
-    default: 0,
-  },
-  category: {
-    type: String,
-    required: [true, 'A gig must belong to a category'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'A gig must have a price'],
-  },
-  cover: {
-    type: String,
-    required: [true, 'A gig must have a cover image'],
-  },
-  images: {
-    type: [String],
-  },
-  shortTitle: {
-    type: String,
-    required: [true, 'A gig must have a short title'],
-  },
-  shortDesc: {
-    type: String,
-    required: [true, 'A gig must have a short description'],
-  },
-  deliveryTime: {
-    type: Number,
-    required: [true, 'A gig must have a delivery time'],
-  },
-  revisionNumber: {
-    type: Number,
-    required: [true, 'A gig must have a revision number'],
-  },
-  features: {
-    type: [String],
-  },
-  sales: {
-    type: Number,
-    default: 0,
-  },
-});
+);
 
 const Gig = mongoose.models.Gig || mongoose.model('Gig', gigSchema);
 
