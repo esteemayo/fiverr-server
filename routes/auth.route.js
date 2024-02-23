47995f6eddda09136605eb92c2bf793d7d255441
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-23 03:28:33 +0100

    feat: add logout route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -9,6 +9,8 @@ router.post('/register', authController.register);
 
 router.post('/login', authController.login);
 
+router.post('/logout', authController.logout);
+
 router.post('/forgot-password', authController.forgotPassword);
 
 router.post('/reset-password/:token', authController.resetPassword);
