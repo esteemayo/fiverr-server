39b4fed023a7baaf2000372d510d779767a526ea
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-02-26 04:16:59 +0100

    feat: handle create order route logic

---
+++ controllers/order.controller.js
@@ -0,0 +1,33 @@
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+
+import Gig from '../models/gig.model.js';
+import Order from '../models/order.model.js';
+
+import { NotFoundError } from '../errors/notFound.js';
+
+export const createOrder = asyncHandler(async (req, res, next) => {
+  const { gigId } = req.params;
+
+  const gig = await Gig.findById(gigId);
+
+  if (!gig) {
+    return next(
+      new NotFoundError(`There is no gig found with that ID → ${gigId}`),
+    );
+  }
+
+  const newOrder = {
+    gig: gig._id,
+    img: gig.cover,
+    title: gig.title,
+    buyerId: req.user.id,
+    sellerId: gig.user,
+    price: gig.price,
+    payment_intent: 'temporary',
+  };
+
+  const order = await Order.create({ ...newOrder });
+
+  res.status(StatusCodes.CREATED).json(order);
+});
--- routes/order.route.js
+++ routes/order.route.js
@@ -1,5 +1,15 @@
 import express from 'express';
 
+import * as authMiddleware from '../middlewares/auth.middleware.js';
+import * as orderController from '../controllers/order.controller.js';
+
 const router = express.Router();
+
+router.post(
+  '/:gigId',
+  authMiddleware.protect,
+  authMiddleware.restrictTo('user'),
+  orderController.createOrder,
+);
 
 export default router;
