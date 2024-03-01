3bbb3d8baf8daadbea95577b99f6d5f3d058664c
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-01 04:25:11 +0100

    fix: modify

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -72,7 +72,7 @@ export const forgotPassword = asyncHandler(async (req, res, next) => {
 
   const html = `
     <div style='background: #f9f9f9; color: #555; padding: 50px; text-align: left;'>
-      <h3>Hi ${user.name},</h3>
+      <h3>Hi ${user.username},</h3>
       <p>There was a request to change your password!</p>
       <p>If you did not make this request then please ignore this email.</p>
       <p>Otherwise, please click this link to change your password: 
