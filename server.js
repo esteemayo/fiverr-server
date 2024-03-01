90dd4bdf8ffaeaa20426647f0b60ab0d3dd679a8
Emmanuel Adebayo <eadebayo15@gmail.com>
2024-03-01 04:41:50 +0100

    fix: modify

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ import connectDB from './config/db.js';
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down...'.red.bold);
   console.log(err.name, err.message);
+  process.exit(1);
 });
 
 app.set('port', process.env.PORT || 8800);
 
 const server = app.listen(app.get('port'), async () => {
   await connectDB();
-  console.log(`App listening on port → ${server.address().port}`.blue.bold);
+  console.log(`App listening on port → ${server.address().port}`.rainbow.bold);
 });
 
 process.on('unhandledRejection', (err) => {
